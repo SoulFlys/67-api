a2d3f7848423d354efcce609c5d916c87968ca77
vipxiaofang <793207918@qq.com>
2017-02-28 18:20:55 +0800

    fileUpload

--- controllers/file.js
+++ controllers/file.js
@@ -16,21 +16,41 @@ module.exports = {
     'POST /admin/file/add': async(ctx, next) => {
         const {files, fields} = await asyncBusboy(ctx.req);
         let file = files[0];    //单图上传
-        let tmpArr = file['filename'].split('.');
-        let ext ='.' + tmpArr[tmpArr.length - 1];
+        let filePath = file.path
+        let fileNameArr = file['filename'].split('.');
+        let ext ='.' + fileNameArr[fileNameArr.length - 1];
         let filename = formatDate(new Date()) + '.' + parseInt(Math.random() * 100) + ext;
-        let stream = fs.createWriteStream(path.join(path.resolve(__dirname,'../uploads/images'), filename));
-        file.pipe(stream);
-        let result = await new File({name:filename,path:stream.path}).save();
-        // console.log(file);
-        // console.log(stream.path)
-        // console.log(result)
-        ctx.rest({
-            id: result._id,
-            name:filename,
-            path:'/images/' + filename,
-            url:baseUrl + filename
-        });
+        let savePath = path.resolve(__dirname,'../uploads/images');
+
+        if (!fs.existsSync(savePath)) {
+            await fs.mkdirSync(path.resolve(__dirname,'../uploads/'));
+            await fs.mkdirSync(path.resolve(__dirname,'../uploads/images/'));
+        }
+
+        let newPath = path.join(savePath, filename);
+        let readAndWrite = () => {
+            return new Promise((resolve, reject) => {
+                fs.readFile(filePath,(err,buff)=>{
+                    if(err){reject(err)}
+                    fs.writeFile(newPath,buff,(err)=>{
+                        if(err){reject(err)}
+                        resolve(true)
+                    })
+                })
+            });
+        }
+
+        let res = await readAndWrite();
+
+        if(res === true){
+            let result = await new File({name:filename,path:filePath,url:baseUrl+filename}).save();
+            ctx.rest({
+                id: result._id,
+                name:filename,
+                path:'/images/' + filename,
+                url:baseUrl + filename
+            });
+        }
     },
     'POST /admin/file/delete': async(ctx,next) => {
         let id = ctx.request.body.id;
